2e4d777b4344b7ec2af334168ebc8ee180c50148
Allan César <acmlira@gmail.com>
2019-05-10 13:48:45 -0300

    Minor changes in webpage
    - Fru frus
    - :nail_care:

--- client/data/src/script.js
+++ client/data/src/script.js
@@ -1,10 +1,22 @@
-var elementsWorthCopying = document.querySelectorAll(".copy-container .copy");
-// Add click event listener for clicking the text
-var styleSheetWrapperEl = document.getElementById('js-copy-container');
 setTimeout(function() {
   document.getElementById("refresh").classList.remove('hide')
 },5000);
 
+var myArray = [
+  "A wonderful",
+  "Beautiful tone of",
+  "A majestic",
+  "A classic tone of",
+  "It's a very nice"
+];
+
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+document.getElementById('random-message').innerHTML = randomItem;
+
+var elementsWorthCopying = document.querySelectorAll(".copy-container .copy");
+// Add click event listener for clicking the text
+var styleSheetWrapperEl = document.getElementById('js-copy-container');
 styleSheetWrapperEl.addEventListener('click', function (e) {
   if (e.target.className !== 'copy') return;
   var element = e.target;
